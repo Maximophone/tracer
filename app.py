6d1c76c8fe2a968451fab2ae0ae74d1bd2cdf1b9
Maximux <fournes.maxime@gmail.com>
2017-03-28 23:35:46 +0100

    Added labels, tag selector and tag cycling with keyboard

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import sqlite3
+
+DATABASE = "db/database.db"
 
 app = Flask(__name__)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
 @app.route("/ping")
 def ping():
@@ -10,5 +26,11 @@ def ping():
 def index():
     return render_template("index.html")
 
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
-app.run()
+if __name__ == "__main__":
+    app.run()
--- static/tracer.js
+++ static/tracer.js
@@ -4,19 +4,16 @@ var width = 960,
 var modes = {
     POINT: "point",
     SPLINE: "spline",
+    LINE: "line",
     IMAGE: "image"};
 
-var tag = "tag0";
+var tag = "Po";
 var current_mode = modes.POINT;
 
 var data = {'point':{},'spline':{}};
     
-// var points = [];
-
 var dragged = null,
     selected = null;
-
-// var line = d3.svg.line();
 
 var svg = d3.select(".tracerzone").append("svg")
     .attr("width", width)
@@ -25,8 +22,46 @@ var svg = d3.select(".tracerzone").append("svg")
 
 add_rect(svg);
 
-d3.select(".tag")
-    .on("change", update_tag);
+labels = [
+	"Po",
+	"Ar",
+	"Ba",
+	"Post Ra",
+	"Go",
+	"Ante Ra",
+	"LIA",
+	"B",
+	"Pog",
+	"Gn",
+	"Me",
+	"LIE",
+	"UIE",
+	"UIA",
+	"ENA",
+	"Or",
+	"Na",
+	"Se",
+	"S",
+	"ENP",
+	"A"
+    ];
+
+label_cycle = {};
+
+for(var i = 0; i<labels.length; i++){
+    label_cycle[labels[i]] = labels[(i+1)%labels.length];
+}
+
+// d3.select(".tag")
+//     .on("change", update_tag);
+
+d3.select("#tag")
+    .on("change", update_tag)
+  .selectAll("option")
+    .data(labels)
+    .enter().append("option")
+    .attr("value", function(d) { return d; })
+    .text(function(d) { return d; });
 
 d3.select("#mode")
     .on("change", update_mode)
@@ -41,7 +76,8 @@ d3.select("#mode")
 
 d3.select(window)
     .on("mousemove", mousemove)
-    .on("mouseup", mouseup);
+    .on("mouseup", mouseup)
+    .on("keydown", keydown);
 
 function mousemove() {
     if (!dragged) return;
@@ -62,34 +98,13 @@ function update_mode(){
     redraw_svg(svg,data,current_mode,tag);
 }
 
-function update_tag(){
-    tag = this.value;
+function update_tag(value){
+    tag = value;
+    if(tag===undefined){
+	tag = this.value;
+    }
     redraw_svg(svg,data,current_mode,tag);
 }
-
-// d3.select(window)
-//     .on("mousemove", mousemove)
-//     .on("mouseup", mouseup)
-//     .on("keydown", keydown);
-
-// d3.select("#interpolate")
-//     .on("change", change)
-//     .selectAll("option")
-//     .data([
-// 	"linear",
-// 	"step-before",
-// 	"step-after",
-// 	"basis",
-// 	"basis-open",
-// 	"basis-closed",
-// 	"cardinal",
-// 	"cardinal-open",
-// 	"cardinal-closed",
-// 	"monotone"
-//     ])
-//     .enter().append("option")
-//     .attr("value", function(d) { return d; })
-//     .text(function(d) { return d; });
 
 function mousedown() {
 
@@ -111,6 +126,25 @@ function mousedown() {
   redraw_svg(svg,data,current_mode,tag);
 }
 
+function keydown() {
+  switch (d3.event.keyCode) {
+    case 65:
+      console.log("pressed");
+      new_tag = label_cycle[tag];
+      tag_box = d3.select('#tag')
+          .property("value",new_tag);
+
+      update_tag(new_tag);
+    // case 8: // backspace
+    // case 46: { // delete
+    //   var i = points.indexOf(selected);
+    //   points.splice(i, 1);
+    //   selected = points.length ? points[i > 0 ? i - 1 : 0] : null;
+    //   redraw();
+      break;
+  }
+}
+
 function add_rect(svg){
     rect = svg.append("rect")
 	.attr("width", width)
@@ -122,6 +156,7 @@ function add_rect(svg){
 function clear_svg(svg){
     svg.selectAll("circle").remove();
     svg.selectAll("path").remove();
+    svg.selectAll("text").remove();
     return rect;
 }
 
@@ -139,12 +174,22 @@ function redraw_svg(svg, data, mode, tag){
 	    .data(restricted_data, function(d){return d;});
 	circle.enter()
 	    .append("circle")
-	    .attr("r",2)
+	    .attr("r",3)
 	    .attr("cx", function(d){return d[0];})
 	    .attr("cy", function(d){return d[1];})
 	    .classed("selected", function(d){
 		return d[2]==tag;});
 	circle.exit().remove();
+	
+	var label = svg.selectAll("text")
+	    .data(restricted_data, function(d){return d;});
+	label.enter()
+	    .append("text")
+	    .attr("x", function(d){return d[0];})
+	    .attr("y", function(d){return d[1];})
+	    .text(function(d){return d[2];});
+	label.exit().remove();
+
 	break;
 
 	case modes.SPLINE:
@@ -162,7 +207,7 @@ function redraw_svg(svg, data, mode, tag){
 
 	    circle.enter().append("circle")
 	        .attr("class","tag-"+key)
-	        .attr("r",2)
+	        .attr("r",5)
 	        .on("mousedown", function(d) { selected = dragged = d; })
 
 	    circle.classed("selected", function(d) { return d === selected; })
@@ -176,85 +221,5 @@ function redraw_svg(svg, data, mode, tag){
 	
     }
 }
-
-// function draw_svg(svg,mode){
-//     switch(mode){
-// 	case modes.SPLINE:
-// 	    svg.append("path")
-// 	        .datum(points)
-// 	        .attr("class", "line")
-// 	        .call(redraw);
-// 	    break;
-//     }
-
-
-//     svg.node().focus();
-
-//     function redraw() {
-// 	svg.select("path").attr("d", line);
-
-// 	var circle = svg.selectAll("circle")
-// 	    .data(points, function(d) { return d; });
-
-// 	circle.enter().append("circle")
-// 	    .attr("r", 1e-6)
-// 	    .on("mousedown", function(d) { selected = dragged = d; redraw(); })
-// 	    .transition()
-// 	    .duration(750)
-// 	    .ease("elastic")
-// 	    .attr("r", 6.5);
-
-// 	circle
-// 	    .classed("selected", function(d) { return d === selected; })
-// 	    .attr("cx", function(d) { return d[0]; })
-// 	    .attr("cy", function(d) { return d[1]; });
-
-// 	circle.exit().remove();
-
-// 	if (d3.event) {
-// 	    d3.event.preventDefault();
-// 	    d3.event.stopPropagation();
-// 	}
-//     }
-
-//     function change() {
-// 	line.interpolate(this.value);
-// 	redraw();
-//     }
-
-//     function mousedown() {
-// 	points.push(selected = dragged = d3.mouse(svg.node()));
-// 	redraw();
-//     }
-
-//     function mousemove() {
-// 	if (!dragged) return;
-// 	var m = d3.mouse(svg.node());
-// 	dragged[0] = Math.max(0, Math.min(width, m[0]));
-// 	dragged[1] = Math.max(0, Math.min(height, m[1]));
-// 	redraw();
-//     }
-
-//     function mouseup() {
-// 	if (!dragged) return;
-// 	mousemove();
-// 	dragged = null;
-//     }
-
-//     function keydown() {
-// 	if (!selected) return;
-// 	switch (d3.event.keyCode) {
-// 	case 8: // backspace
-// 	case 46: { // delete
-// 	    var i = points.indexOf(selected);
-// 	    points.splice(i, 1);
-// 	    selected = points.length ? points[i > 0 ? i - 1 : 0] : null;
-// 	    redraw();
-// 	    break;
-// 	}
-// 	}
-//     }
-
-// }
 
 console.log("done");
